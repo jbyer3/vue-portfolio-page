83fca28479215ff8017cbecad42e1cd638369f08
Jason Byer <jbyer3@gmail.com>
2019-03-25 14:24:51 -0400

    test

--- src/components/CurtainCall.vue
+++ src/components/CurtainCall.vue
@@ -6,7 +6,7 @@
     it went okay, maybe you should checkout this 
   </div>
 
-
+<!-- 
   <div class="app-card">
     <h4>app one</h4>
     <a rel="stylesheet" href="https://tax-calc-app.herokuapp.com/">
@@ -14,7 +14,18 @@
         <img src="@/assets/tax-calc-app.png" alt="project one">
       </div>
     </a>
+  </div> -->
+
+<div v-for="project in projects" :key="project.id" class="app-card">
+    <h4>{{ project.title }}</h4>
+    <img :src="project.image" :alt="project.description">
+    <a rel="stylesheet" :href="project.link">
+      <div class="img-holder">
+        <img :src="project.image" v-bind:alt="project.description">
+      </div>
+    </a>
   </div>
+
 </div>
 </template>
 
@@ -29,14 +40,14 @@ export default {
       {
         title: 'tax calc',
         id: 11,
-        image: '@/assets/tax-calc-app.png',
+        image: './assets/tax-calc-app.png',
         description: 'todo',
         focus: 'vue frontend framework'
       },
       {
         title: 'tree view',
         id: 22,
-        image: '@/assets/passport-tree.png',
+        image: '../assets/passport-tree.png',
         description: '',
         focus: 'reactive content without a JS Framework, vanilla JS only.'
       },
