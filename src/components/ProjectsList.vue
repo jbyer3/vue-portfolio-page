636e01992243b9790bd4e41dc0030a6ac9a6d6f7
Jason Byer <jbyer3@gmail.com>
2019-03-30 19:40:58 -0400

    add orange underline to project titles

--- src/components/ProjectsList.vue
+++ src/components/ProjectsList.vue
@@ -6,14 +6,17 @@
   <div v-for="project in projects" :key="project.id" class="app-card">
     
     <!-- <img :src="project.image" :alt="project.description"> -->
+    <!-- <div class="project-title">
       <h3>{{ project.title }}</h3>
-      <div class="img-holder">
-        <a :href="project.link">
-          <img :src="project.image" v-bind:alt="project.description">
-        </a>
-        <p><span class="img-cap-title">description: </span>{{project.description}}</p>
-        <p><span class="img-cap-title">focus: </span>{{project.focus}}</p>
-      </div>
+    </div> -->
+    <h3 class="project-title">{{project.title}}</h3>
+    <div class="img-holder">
+      <a :href="project.link">
+        <img :src="project.image" v-bind:alt="project.description">
+      </a>
+      <p><span class="img-cap-title">description: </span>{{project.description}}</p>
+      <p><span class="img-cap-title">focus: </span>{{project.focus}}</p>
+    </div>
   </div>
 
 </div>
@@ -79,4 +82,9 @@ img {
   background: $light-blue;
 }
 
+.project-title {
+  border-bottom: 0.2rem solid $orange;
+  width: 10rem;
+  // text-
+}
 </style>
